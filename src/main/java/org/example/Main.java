18fad08fc06947440e2a8a579f6029971a6526cc
Joseli Ramos <josielisouzaderamos714@gmail.com>
2024-08-09 10:16:58 -0300

    first commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,39 @@
+package org.example;
+
+import com.systemOrderService.entities.enums.Service;
+import com.systemOrderService.model.entities.LegalPerson;
+import com.systemOrderService.model.entities.Person;
+import com.systemOrderService.repositories.services.OrderService;
+import com.systemOrderService.repositories.services.Services;
+
+public class Main {
+    public static void main(String[] args) {
+
+//        String number = ;
+//
+//        person.FormattedNumber(number);
+
+//
+//        Person l;
+//
+//       l = new Person("Maria", "...", "..", "kkk", "llll");
+//
+//
+//        l.addPerson(l);
+//        System.out.print(l.getListPerson().size());
+
+//        XX. XXX. XXX/0001-XX
+//        String cnpj = "12344555666688";
+//
+//       Person p = new Person();
+//
+//       p.formattedCnpj(cnpj);
+
+//        OrderService or = new OrderService();
+//        Services ser = new Services("tela", "SUPORTE");
+//
+//        ser.newService();
+//        System.out.print(or.total(ser.getPrice(),3));
+
+    }
+}
