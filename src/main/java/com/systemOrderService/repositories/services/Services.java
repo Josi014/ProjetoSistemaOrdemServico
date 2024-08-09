251fcb1b9439649b123e65c14935c524dd47ee8c
Joseli Ramos <josielisouzaderamos714@gmail.com>
2024-08-09 00:07:10 -0300

    Creating newService method to add values ​​according to the corresponding enum

---
+++ src/main/java/com/systemOrderService/repositories/services/Services.java
@@ -0,0 +1,55 @@
+package com.systemOrderService.repositories.services;
+
+import com.systemOrderService.entities.enums.Service;
+
+public class Services {
+    private String defect;
+    private Service service;
+    private Double value;
+
+
+    public Services(String defect,String service) {
+        this.defect = defect;
+        this.service = Service.valueOf(service);
+    }
+
+    public String getDefect() {
+        return defect;
+    }
+
+    public void setDefect(String defect) {
+        this.defect = defect;
+    }
+
+    public Service getService() {
+        return service;
+    }
+
+    public void setService(Service service) {
+        this.service = service;
+    }
+
+    public Double getValue() {
+        return value;
+    }
+
+    public void newService() {
+        switch (service) {
+            case SUPORTE:
+                value += 80.00;
+                break;
+            case LIMPEZA:
+                value += 50.00;
+                break;
+            case FORMATAÇÃO:
+                value+=110.00;
+                break;
+            case BACKUP:
+                value+=55.00;
+        }
+    }
+
+    public double total(double price, double amount) {
+        return amount * price;
+    }
+}
