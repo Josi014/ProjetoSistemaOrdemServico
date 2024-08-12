be6f3e519a2e6248dbb494b0da1a45651d12397d
Josiéli de Souza de Ramos <josielisouzaderamos714@gmail.com>
2024-08-12 18:15:41 -0300

    kotlin enum to java enum

--- src/main/java/com/systemOrderService/entities/enums/Products.java
+++ src/main/java/com/systemOrderService/entities/enums/Products.java
@@ -1,5 +1,13 @@
 package com.systemOrderService.entities;
 
 public enum Products {
-
+    SSD,
+    MEMÓRIA,
+    IMPRESSORA,
+    TECLADO,
+    MOUSE,
+    SWITCH8P,
+    SWITCH5P,
+    ROTEADOR,
+    TELA;
 }
