f27f86aba2574cfc5df2897553eb96efb378274c
Joseli Ramos <josielisouzaderamos714@gmail.com>
2024-08-11 22:41:57 -0300

    Exception handled

--- src/main/java/com/systemOrderService/model/entities/LegalPerson.java
+++ src/main/java/com/systemOrderService/model/entities/LegalPerson.java
@@ -1,22 +1,21 @@
 package com.systemOrderService.model.entities;
 
-import com.systemOrderService.repositories.services.OrderService;
-
-import java.util.ArrayList;
-import java.util.List;
+import com.systemOrderService.model.Exception.DomainException;
 
 public class LegalPerson extends Person{
     private String reason;
-    private int cnpj;
+    private String cnpj;
     private String stateRegistration;
-    private List<OrderService> listNewOrder = new ArrayList<>();
 
     public LegalPerson(){
         super();
     }
 
-    public LegalPerson(String name, String email, String adress, String telephone, String zipCode, String reason, int cnpj, String stateRegistration) {
-        super(name, email, adress, telephone, zipCode);
+    public LegalPerson(String name, String email, String address, String telephone, String zipCode, String reason, String cnpj, String stateRegistration) throws DomainException {
+        super(name, email, address, telephone, zipCode);
+        if(cnpj == null || cnpj.length() != 14){
+            throw new DomainException("Error cnpj cannot be greater or less than 14 characters or.");
+        }
         this.reason = reason;
         this.cnpj = cnpj;
         this.stateRegistration = stateRegistration;
@@ -30,11 +29,11 @@ public class LegalPerson extends Person{
         this.reason = reason;
     }
 
-    public int getCnpj() {
+    public String getCnpj() {
         return cnpj;
     }
 
-    public void setCnpj(int cnpj) {
+    public void setCnpj(String cnpj) {
         this.cnpj = cnpj;
     }
 
@@ -46,27 +45,18 @@ public class LegalPerson extends Person{
         this.stateRegistration = stateRegistration;
     }
 
-    public List<OrderService> getListNewOrder() {
-        return listNewOrder;
-    }
-
-    public void formattedCnpj(String cnpj) {
-        String formattedCnpj = String.format("%s.%s.%s/%s-%s",
+    public String formattedCnpj(){
+        return String.format("%s.%s.%s/%s-%s",
                 cnpj.substring(0, 2),
                 cnpj.substring(2, 5),
                 cnpj.substring(5,8),
                 cnpj.substring(8,12),
                 cnpj.substring(12,14));
-        System.out.print(formattedCnpj);
     }
 
-    public void addOrderService(OrderService order) {
-        listNewOrder.add(order);
-        order.setLegalPerson(this);
-    }
 
-    public void removeOrderService(OrderService order) {
-        listNewOrder.remove(order);
-        order.setLegalPerson(null);
+    @Override
+    public String showInfoPerson(){
+        return super.showInfoPerson() + "\nCNPJ: " + formattedCnpj()+"\n";
     }
 }
